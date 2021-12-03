d8498ae90ceb0cc71c6a8ca41853a23aa641066d
David Landa <david.landa@protonmail.com>
2021-12-03 13:21:21 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@
 @rem This script convert a current directory with markdown files to HTML.
 @rem The output files are named after the source files.
 
-pandoc index.md -f markdown -t html -s -o index.html -c style.css
+pandoc index.md -f markdown -t html -s -o index.html -c style.css --metadata title="groundf"
